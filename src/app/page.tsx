70da27530c559e7f7aa54915118cfb27306b27f6
Yaroslav1979 <yarico.rv@gmail.comm>
2025-01-21 18:54:01 +0200

    postCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-// import clsx from 'clsx';
-// import StatusLabel, { Status } from './components/status-label';
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <h1 className="text-xl">Home page</h1>
-//         <StatusLabel status={Status.Active} disabled={false}>Active</StatusLabel>
-//         <StatusLabel status={Status.NotActive} disabled={false}>Not Active</StatusLabel>
-//         <StatusLabel status={Status.Pending} disabled={false}>Pending</StatusLabel>
-//         <StatusLabel status={Status.Suspended} disabled={false}>Suspended</StatusLabel>
-//     </main>
-//   );
-// }
+import '../app/globals.css';
+import clsx from 'clsx';
+import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
-    </div>
+    <main>
+      <h1 className="text-xl">Home page</h1>
+        <StatusLabel status={Status.Active} disabled={false}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive} disabled={false}>Not Active</StatusLabel>
+        <StatusLabel status={Status.Pending} disabled={false}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended} disabled={false}>Suspended</StatusLabel>
+      <div className="text-red-500 text-2xl">Hello Tailwind!</div>
+    </main>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white">
+//       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+//     </div>
+//   );
+// }
